aef207a6178751040d1cbbb81aec4894e1b4f071
ert-git <levina.olga.s@yandex.ru>
2020-04-01 14:24:05 +0500

    home work #1 - gradle

---
+++ m1-gradle/src/main/java/ru/otus/edu/levina/Person.java
@@ -0,0 +1,15 @@
+package ru.otus.edu.levina;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+
+}
